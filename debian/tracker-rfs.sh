fdbad0d80ae35f7ee970a71f501f8887a03f702c
Carlos Garnacho <carlosg@gnome.org>
2009-04-30 11:50:06 +0200

    Add initial version of debian/ directory for Fremantle.

---
+++ debian/tracker-rfs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Ensure tracker is stopped
+/etc/osso-af-init/tracker.sh stop
+
+# Remove content databases
+rm -rf /home/user/.cache/tracker/
+
+# Remove user databases, logs and lock files
+rm -rf /home/user/.local/share/tracker/
+
+# Remove the configuration (recreated on next start)
+rm -rf /home/user/.config/tracker/
